f36f59820d3014f02b994fc757241b4ce1ed7921
gabrielabiah <gabriel@manifestghana.com>
2025-07-14 20:55:52 +0000

    added printTeacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,16 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Define the interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Implement the function
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Test the function
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
